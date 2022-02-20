f69aa4a8ee787fe5c246542f4a4a991f84e9d5cf
Stratia <97822999+Stratia@users.noreply.github.com>
2022-02-20 15:14:06 -0800

    Add files via upload

---
+++ src/credentials.py
@@ -0,0 +1,25 @@
+
+"""
+Tweepy credentials
+"""
+
+access_token = ""
+access_token_secret = ""
+
+consumer_key = ""
+consumer_secret = ""
+
+"""
+List for top 30 Crypto Currentcies according to https://coinmarketcap.com/
+"""
+
+cryptos_full_name= ["Bitcoin","Ethereum","Tether","BNB","USD Coin","XRP",
+                        "Cardano","Solana","Terra","Avalanch","Dogecoin","Polkadot",
+                        "Binance USD","Shiba Inu","Polygon","Crypto.com Coin","TerraUSD",
+                        "Wrapped Bitcoin","Dai","Litecoin","Cosmos","Chainlink","Uniswap",
+                        "NEAR Protocol","TRON","Bitcoin Cash","FTX Token","Algorand",
+                        "UNUS SED LEO","Decentraland"]
+
+crypto_acronym = ["BTC","ETH","USDT","USDC","ADA","SOL","LUNA","AVAX","DOGE","DOT",
+                      "BUSD","SHIB","MATIC","CRO","WBTC","DAI","LTC","ATOM","LINK","UN",
+                      "NEAR","TRX","BCH","FTT","ALGO","LEO","MANA"]
